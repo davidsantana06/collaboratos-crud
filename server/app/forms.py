d37e99ebfa67962d6f541f9d0072ae898f0ed57d
davidsantana06 <davidsantana06@hotmail.com>
2024-02-19 14:10:16 -0300

    feat(server): add create collaborator form

---
+++ server/app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import FloatField, StringField
+from wtforms.validators import DataRequired, Length, NumberRange
+
+
+class CreateCollaboratorForm(FlaskForm):
+    first_name = StringField(validators=[DataRequired(), Length(1, 30)])
+    last_name = StringField(validators=[DataRequired(), Length(1, 30)])
+    participation = FloatField(validators=[DataRequired(), NumberRange(1, 100)])
