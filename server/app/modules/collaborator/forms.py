7818be1ae0504be507fae01bf0e776a904af1109
davidsantana06 <davidsantana06@hotmail.com>
2024-02-20 17:57:38 -0300

    feat(server): add form input names

--- server/app/modules/collaborator/forms.py
+++ server/app/modules/collaborator/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class CreateForm(FlaskForm):
-    first_name = StringField(validators=[DataRequired(), Length(1, 30)])
-    last_name = StringField(validators=[DataRequired(), Length(1, 30)])
-    participation = FloatField(validators=[DataRequired(), NumberRange(1, 100)])
+    first_name = StringField(name="first-name", validators=[DataRequired(), Length(1, 30)])
+    last_name = StringField(name="last-name", validators=[DataRequired(), Length(1, 30)])
+    participation = FloatField(name="participation", validators=[DataRequired(), NumberRange(1, 100)])
